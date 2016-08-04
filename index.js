7b84d7a692e99251afa2023878a9e48ca479f192
khacpham <khacpv@gmail.com>
2016-08-04 23:01:25 +0700

    change config

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-var port = process.env.PORT || 8080;
+
+var ip = process.env.OPENSHIFT_NODEJS_IP || '192.168.1.102';
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
 server.listen(port, function () {
   console.log('Server listening at port %d', port);
