86f012576e572495ee8197bdaa52e6801540616d
khacpham <khacpv@gmail.com>
2016-09-04 12:44:20 +0700

    add db

--- mongodb/mongodb.js
+++ mongodb/mongodb.js
@@ -4,9 +4,9 @@ var Mongo = {};
 
 Mongo.IP = process.env.OPENSHIFT_MONGODB_DB_HOST || '127.0.0.1';
 Mongo.PORT = process.env.OPENSHIFT_MONGODB_DB_PORT || '52086';
-Mongo.dbName = process.env.OPENSHIFT_APP_NAME || 'image';
+Mongo.dbName = process.env.OPENSHIFT_APP_NAME || 'altp';
 Mongo.user = process.env.OPENSHIFT_MONGODB_DB_USERNAME || 'admin';
-Mongo.pass = process.env.OPENSHIFT_MONGODB_DB_PASSWORD || 'T3k_111XG72d';
+Mongo.pass = process.env.OPENSHIFT_MONGODB_DB_PASSWORD || 'Jnav2De-XvhE';
 
 Mongo.connectionString = function () {
     return Mongo.user + ":" +
@@ -16,12 +16,13 @@ Mongo.connectionString = function () {
         Mongo.dbName;
 };
 
-var db = mongojs(Mongo.connectionString(), ['altp','users']);
+var db = mongojs(Mongo.connectionString(), ['altp','users', 'questions']);
 var altp = db.collection('altp');
 var users = db.collection('users');
+var questions = db.collection('questions');
 
 Mongo.db = db;
 Mongo.altp = altp;
-Mongo.users = users;
+Mongo.questions = questions;
 
 module.exports = Mongo;
